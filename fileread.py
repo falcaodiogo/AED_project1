186cb0cbca79b917ed6c8b47a813a003ddd21250
zegameiro <jose.mcgameiro@ua.pt>
2022-12-05 18:38:04 +0000

    Nothing

--- fileread.py
+++ fileread.py
@@ -27,19 +27,19 @@ with open("FileOutput") as file:
 
     print("\nDICT1\n")
     for item, amount in dic1.items():  
-        print("{} : {}".format(item, amount))
+        print(amount, end=" ")
 
     print("\nDICT2\n")
     for item, amount in dic2.items():  
-        print("{} : {}".format(item, amount))
+        print(amount, end=" ")
 
     print("\nDICT3\n")
     for item, amount in dic3.items():  
-        print("{} : {}".format(item, amount))
+        print(amount, end=" ")
 
     print("\nDICT4\n")
     for item, amount in dic4.items():  
-        print("{} : {}".format(item, amount))
+        print(amount, end=" ")
 
 ## MATPLOTLIB
 # import matplotlib.pyplot as plt
